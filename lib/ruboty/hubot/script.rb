b38807e27923297acc8a71a50add1e9a7a75e9ed
block_given? <block_given@outlook.com>
2014-10-16 01:42:00 +0900

    Use coffee-script gem

--- lib/ruboty/hubot/script.rb
+++ lib/ruboty/hubot/script.rb
@@ -1,40 +1,19 @@
 require 'mem'
-require 'execjs'
-require "open-uri"
+require 'coffee-script'
 
 module Ruboty
   module Hubot
     class Script
-      class << self
-        include Mem
-
-        def compile(source)
-          coffee.call("CoffeeScript.compile", source, bare: true)
-        end
-
-        private
-
-        def coffee
-          ExecJS.compile(coffee_source)
-        end
-        memoize :coffee
-
-        def coffee_source
-          open("http://coffeescript.org/extras/coffee-script.js").read
-        end
-        memoize :coffee_source
-      end
-
       def initialize(source_path)
         if File.extname(source_path).downcase == '.coffee'
-          @source = self.class.compile(File.read(source_path))
+          @source = CoffeeScript.compile(File.read(source_path), bare: true)
         else
           @source = File.read(source_path)
         end
       end
 
       def to_s
-        @source
+        @source.to_s
       end
     end
   end
--- ruboty-hubot.gemspec
+++ ruboty-hubot.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
 
   spec.add_runtime_dependency "ruboty"
   spec.add_runtime_dependency "execjs"
+  spec.add_runtime_dependency "coffee-script"
   spec.add_development_dependency "bundler", "~> 1.7"
   spec.add_development_dependency "rake", "~> 10.0"
   spec.add_development_dependency "pry"
