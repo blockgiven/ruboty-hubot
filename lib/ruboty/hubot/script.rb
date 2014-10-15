0bf2612ee816b2e0b9cd03310480bf31bf333d9b
block_given? <block_given@outlook.com>
2014-10-15 23:43:48 +0900

    support .coffee

--- lib/ruboty/hubot/script.rb
+++ lib/ruboty/hubot/script.rb
@@ -1,9 +1,36 @@
+require 'mem'
+require 'execjs'
+require "open-uri"
+
 module Ruboty
   module Hubot
     class Script
-      # TODO: compile .coffee
+      class << self
+        include Mem
+
+        def compile(source)
+          coffee.call("CoffeeScript.compile", source, bare: true)
+        end
+
+        private
+
+        def coffee
+          ExecJS.compile(coffee_source)
+        end
+        memoize :coffee
+
+        def coffee_source
+          open("http://coffeescript.org/extras/coffee-script.js").read
+        end
+        memoize :coffee_source
+      end
+
       def initialize(source_path)
-        @source = File.read(source_path)
+        if File.extname(source_path).downcase == '.coffee'
+          @source = self.class.compile(File.read(source_path))
+        else
+          @source = File.read(source_path)
+        end
       end
 
       def to_s
